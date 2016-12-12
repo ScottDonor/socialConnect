262fc0d832ccedde6d8981e53b10a996d285961e
ScottDonor <themetaldonor@hotmail.com>
2016-12-12 13:49:17 +1030

    Add files via upload

---
+++ templates/template_terms.php
@@ -0,0 +1,6 @@
+    <div id="terms" style="display:none;">
+      <h3>Web Intersect Terms Of Use</h3>
+      <p>1. Play nice here.</p>
+      <p>2. Take a bath before you visit.</p>
+      <p>3. Brush your teeth before bed.</p>
+    </div>
