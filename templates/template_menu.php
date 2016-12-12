aa77007adbf014b9bfd59114846b39672c71958e
ScottDonor <themetaldonor@hotmail.com>
2016-12-12 13:16:35 +1030

    Add files via upload

---
+++ templates/template_menu.php
@@ -0,0 +1,6 @@
+          <a href="login.php">
+            <img src="images/home.png" alt="home" title="Home"  height="30" width="30">
+          </a>
+		  
+          <a href="notifications.php"><?php echo $notificationsthere?></a>
+          <a href="#">Menu_Item_2</a>
