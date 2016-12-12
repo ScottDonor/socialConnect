5fa1daf50c7ca79c09556a535f6d1bb2a7c3e7f1
ScottDonor <themetaldonor@hotmail.com>
2016-12-12 14:26:55 +1030

    Add files via upload

--- templates/template_menu.php
+++ templates/template_menu.php
@@ -11,12 +11,7 @@ $numrows = mysqli_num_rows($query);
 $sql2 = "SELECT * FROM friends WHERE user2='$log_username' AND accepted='0' ORDER BY datemade ASC";
 $query2 = mysqli_query($db_conx, $sql2);
 $numrows2 = mysqli_num_rows($query2);
-
-if($numrows < 1||$numrows2 < 1){
-	$notificationsthere="Notifications (".($numrows + $numrows2).")";
-} else {
-	$notificationsthere = "Notifications";
-}
+$notificationsthere = "Notifications (".($numrows + $numrows2).")";
 ?> 
 
           <a href="index.php">Home</a>
