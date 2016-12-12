ca8ccde72ac5c756473cc5ff839923c38bf1bc03
ScottDonor <themetaldonor@hotmail.com>
2016-12-12 16:34:52 +1030

    Add files via upload

--- templates/template_pageBottom.php
+++ templates/template_pageBottom.php
@@ -1,1 +1,9 @@
-<div id="pageBottom">&copy;<?php echo date('Y');?> Social Connect Made By RAHUL MEHRA</div>
+<div id="pageBottom">&copy;<?php echo date('Y');?> Social Connect Made By RAHUL MEHRA<br/>
+<?php
+	$sql="select * from users ORDER BY RAND() limit 100";
+	$result = mysqli_query($db_conx, $sql);
+	$row_cnt = $result->num_rows;
+	$result->close();
+?>
+<?php echo $row_cnt; ?> Registered Members!
+</div>
